13b90c58f615052277d8b908d2bb796990e17abd
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2024-02-16 11:50:52 -0700

    Add right or left input

--- src/app/src/app/components/characters/player/player.component.ts
+++ src/app/src/app/components/characters/player/player.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-player',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './player.component.html',
-  styleUrl: './player.component.scss'
+  styleUrl: './player.component.scss',
 })
 export class PlayerComponent {
-
+  @Input() directionFacing: 'Right' | 'Left' = 'Right';
 }
