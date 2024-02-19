7f808cbe5ee0877966ffc265eb21039ff96415a7
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2024-02-19 07:45:56 -0700

    test

--- src/app/components/game/game.component.ts
+++ src/app/components/game/game.component.ts
@@ -10,7 +10,7 @@ import { Subscription } from 'rxjs';
 import { CommonModule } from '@angular/common';
 
 export type Player = {
-  name: string;
+  name: string; // test
   position: Position;
   beingControlledOnClient: boolean; // False if not your turn and pass and play
   movementSpeed: number;
