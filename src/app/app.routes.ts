b3910b1922d0836443ea3633a3747b4bdb5b7445
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2024-02-22 16:27:00 -0700

    Add button that navigates to login or sign up

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,11 +5,16 @@ import { GameComponent } from './components/game/game.component';
 import { AuthGuard } from './auth/auth.guard';
 import { inject } from '@angular/core';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { LoginOrSignUpComponent } from './auth/components/login-or-sign-up/login-or-sign-up.component';
 
 export const routes: Routes = [
   {
     path: 'landing-page',
     component: LandingPageComponent,
+  },
+  {
+    path: 'login-or-sign-up',
+    component: LoginOrSignUpComponent,
   },
   {
     path: 'home',
--- src/app/components/landing-page/landing-page.component.ts
+++ src/app/components/landing-page/landing-page.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { LoginOrSignUpComponent } from '../../auth/components/login-or-sign-up/login-or-sign-up.component';
 import { AuthService } from '../../auth/auth.service';
+import { MatButtonModule } from '@angular/material/button';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [LoginOrSignUpComponent],
+  imports: [LoginOrSignUpComponent, MatButtonModule],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss',
 })
 export class LandingPageComponent {
-  constructor(protected authService: AuthService) {}
+  constructor(protected authService: AuthService, protected router: Router) {}
 }
