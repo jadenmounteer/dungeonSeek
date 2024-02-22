e0436dec43cd92afa0855e9b24705fa48682daff
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2024-02-21 21:35:40 -0700

    Add a bit of routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { GameComponent } from './components/game/game.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomePageComponent,
+  },
+  {
+    path: 'game',
+    component: GameComponent,
+  },
+];
