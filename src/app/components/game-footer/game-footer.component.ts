508e40a5ffd5f9aff87c34d5be9ca3670771af43
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2024-03-04 21:53:15 -0700

    Add some styles

--- src/app/components/game-footer/game-footer.component.ts
+++ src/app/components/game-footer/game-footer.component.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-game-footer',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './game-footer.component.html',
   styleUrl: './game-footer.component.scss',
 })
