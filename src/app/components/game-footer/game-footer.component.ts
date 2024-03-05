b896a8140e96bbb06a6bc1ee36c488b71cb3fef8
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2024-03-04 22:37:18 -0700

    Add dynamic turn number

--- src/app/components/game-footer/game-footer.component.ts
+++ src/app/components/game-footer/game-footer.component.ts
@@ -10,4 +10,5 @@ import { Component, Input } from '@angular/core';
 })
 export class GameFooterComponent {
   @Input() public characterMovementSpeed: number | undefined;
+  @Input() public turnNumber!: number;
 }
