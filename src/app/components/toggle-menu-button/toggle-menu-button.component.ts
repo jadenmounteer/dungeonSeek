81a8937f6b905d1b6a162b554e9e67607196cc27
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2024-03-06 16:29:32 -0700

    Begin adding button

--- src/app/components/toggle-menu-button/toggle-menu-button.component.ts
+++ src/app/components/toggle-menu-button/toggle-menu-button.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-toggle-menu-button',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule, CommonModule],
   templateUrl: './toggle-menu-button.component.html',
-  styleUrl: './toggle-menu-button.component.scss'
+  styleUrl: './toggle-menu-button.component.scss',
 })
 export class ToggleMenuButtonComponent {
+  @Output() public toggleMenu: EventEmitter<any> = new EventEmitter();
+  protected menuOpen: boolean = true;
 
+  protected onToggleMenu() {
+    this.menuOpen = !this.menuOpen;
+    this.toggleMenu.emit();
+  }
 }
