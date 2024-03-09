fc7e6f4f6dc6046594b8e758fad4dff715d9df77
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2024-03-08 18:56:16 -0700

    Begin adding location info data

--- src/app/components/location-info/location-info.component.ts
+++ src/app/components/location-info/location-info.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { LocationNode } from '../../services/location-service';
 
 @Component({
   selector: 'app-location-info',
   standalone: true,
   imports: [],
   templateUrl: './location-info.component.html',
-  styleUrl: './location-info.component.scss'
+  styleUrl: './location-info.component.scss',
 })
 export class LocationInfoComponent {
+  @Input() location: LocationNode | undefined;
 
+  constructor() {}
 }
