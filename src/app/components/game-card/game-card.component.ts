201232081a562332b71a61f717e4b3893ab7b1d5
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2024-03-08 19:36:13 -0700

    Begin changing game card

--- src/app/components/game-card/game-card.component.ts
+++ src/app/components/game-card/game-card.component.ts
@@ -1,12 +1,53 @@
-import { Component } from '@angular/core';
-
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-game-card',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './game-card.component.html',
-  styleUrl: './game-card.component.scss'
+  styleUrl: './game-card.component.scss',
+  animations: [
+    trigger('flipState', [
+      state(
+        'active',
+        style({
+          transform: 'rotateY(179deg)',
+        })
+      ),
+      state(
+        'inactive',
+        style({
+          transform: 'rotateY(0)',
+        })
+      ),
+      transition('active => inactive', animate('500ms ease-out')),
+      transition('inactive => active', animate('500ms ease-in')),
+    ]),
+  ],
 })
-export class GameCardComponent {
+export class GameCardComponent implements OnInit {
+  protected flip: string = 'inactive';
 
+  toggleFlip() {
+    this.flip = this.flip == 'inactive' ? 'active' : 'inactive';
+  }
+
+  ngOnInit(): void {
+    this.flipCard();
+  }
+
+  private flipCard() {
+    // flip the card after 5 seconds
+    setTimeout(() => {
+      // flip the card
+      this.toggleFlip();
+    }, 5000);
+  }
 }
