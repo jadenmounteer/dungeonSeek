24b3df0877d90938f3ec51630b8929522450cd64
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2024-03-12 17:36:46 -0600

    Redo shuffle method

--- src/app/services/card.service.ts
+++ src/app/services/card.service.ts
@@ -51,8 +51,23 @@ export class CardService {
     return this.shuffle(cardNames);
   }
 
-  private shuffle(array: Array<CardName>): Array<CardName> {
-    // shuffle the array
-    return array.sort(() => Math.random() - 0.5);
+  private shuffle(array: CardName[]): CardName[] {
+    let currentIndex = array.length,
+      randomIndex;
+
+    // While there remain elements to shuffle.
+    while (currentIndex > 0) {
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex],
+        array[currentIndex],
+      ];
+    }
+
+    return array;
   }
 }
