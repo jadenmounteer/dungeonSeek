f4b95eeb982da1b310190cd575562b11b8603861
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2024-03-27 16:01:57 -0600

    Begin adding card crieteria object

--- src/app/types/card-deck.ts
+++ src/app/types/card-deck.ts
@@ -5,7 +5,16 @@ export type CardInfo = {
   quantity: number; // The number of times this card appears in the deck.
   discardAfterUse: boolean; // Whether or not to discard the card after use.
   choices: Choice[];
+  cardCriteria: CardCriteria | null; // TODO this will be used to determine if the card can be drawn by the current player. For example, we don't want the orc trophy hunter card to be drawn by a low-level player.
 };
+
+// TODO implement this
+export interface CardCriteria {
+  level: number;
+  gold: number;
+  items: string[];
+  questsCompleted: string[];
+}
 
 // The choices the player can make when drawing this card.
 export interface Choice {
