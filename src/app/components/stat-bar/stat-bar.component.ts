de358556f8533249a38b368d53abb2dce2502bec
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2024-03-30 11:51:08 -0600

    More updates to character stats

--- src/app/components/stat-bar/stat-bar.component.ts
+++ src/app/components/stat-bar/stat-bar.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { CharacterStat } from '../../types/character';
 
+export type StatColor = 'red' | 'blue' | 'green' | 'dark-blue';
 @Component({
   selector: 'app-stat-bar',
   standalone: true,
   imports: [],
   templateUrl: './stat-bar.component.html',
-  styleUrl: './stat-bar.component.scss'
+  styleUrl: './stat-bar.component.scss',
 })
-export class StatBarComponent {
+export class StatBarComponent implements OnInit {
+  @Input() stat!: CharacterStat;
+  @Input() statName!: string;
+  @Input() statColor: StatColor = 'red';
 
+  ngOnInit(): void {
+    if (!this.stat || !this.statName) {
+      throw new Error('StatBarComponent is missing a required input.');
+    }
+  }
 }
