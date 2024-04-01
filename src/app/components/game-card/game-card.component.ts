729b714ab1f3bd4ebb35028cee331af200506b1e
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2024-04-01 17:41:38 -0600

    More styles

--- src/app/components/game-card/game-card.component.ts
+++ src/app/components/game-card/game-card.component.ts
@@ -17,26 +17,7 @@ import { CardInfo, DeckName, Outcome } from '../../types/card-deck';
   imports: [CommonModule],
   templateUrl: './game-card.component.html',
   styleUrl: './game-card.component.scss',
-  animations: [
-    trigger('flipState', [
-      state(
-        'active',
-        style({
-          transform: 'rotateY(179deg)',
-        })
-      ),
-      state(
-        'inactive',
-        style({
-          transform: 'rotateY(0)',
-        })
-      ),
-      transition('active => inactive', animate('500ms ease-out')),
-      transition('inactive => active', animate('500ms ease-in')),
-    ]),
-    fadeIn,
-    fadeOut,
-  ],
+  animations: [fadeIn, fadeOut],
 })
 export class GameCardComponent implements OnInit {
   @Input() public cardName: string | undefined;
