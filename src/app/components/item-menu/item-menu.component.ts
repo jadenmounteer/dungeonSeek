4269bb701ad6d000a551bc1862c177f7f188ebfd
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2024-04-04 16:34:28 -0600

    More changes to item menu

--- src/app/components/item-menu/item-menu.component.ts
+++ src/app/components/item-menu/item-menu.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { CardInfoComponent } from '../card-info/card-info.component';
+import { GameCardComponent } from '../game-card/game-card.component';
+import { MenuComponent } from '../menu/menu.component';
 
 @Component({
   selector: 'app-item-menu',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MenuComponent, GameCardComponent, CardInfoComponent],
   templateUrl: './item-menu.component.html',
-  styleUrl: './item-menu.component.scss'
+  styleUrl: './item-menu.component.scss',
 })
 export class ItemMenuComponent {
+  @Output() public closeMenu = new EventEmitter<any>();
 
+  protected onCloseMenu() {
+    this.closeMenu.emit();
+  }
 }
