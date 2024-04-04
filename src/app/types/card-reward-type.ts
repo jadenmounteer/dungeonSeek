3a9bfe1e6c626e42d2f6140504ffabbb25ec6400
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2024-04-04 17:17:13 -0600

    Modify comment

--- src/app/types/card-reward-type.ts
+++ src/app/types/card-reward-type.ts
@@ -1,2 +1,2 @@
-// If you kill and easy monster or complete an easy dungeon, you get this type of reward.
+// If you kill and easy monster or complete an easy dungeon, you get this type of reward, etc.
 export type CardRewardType = 'Easy' | 'Moderate' | 'Hard' | 'Insane';
