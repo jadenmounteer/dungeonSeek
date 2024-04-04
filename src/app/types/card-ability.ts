e8db0f555801d93a27d2421e05ad81e78118c581
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2024-04-04 17:38:47 -0600

    add passive ability type

--- src/app/types/card-ability.ts
+++ src/app/types/card-ability.ts
@@ -2,14 +2,14 @@
 // while weapons are used on your opponent and spells can be used for both.
 
 export interface CardAbility {
-  name: CardAbilityKey;
+  name: ActionCardAbilityKey | PassiveCardAbilityKey;
   usedAgainstOpponentDescription: String;
   usedAgainstSelfDescription: String;
 }
 
-export type CardAbilityKey = 'Fireball' | 'Heal' | 'Banish';
+export type ActionCardAbilityKey = 'Fireball' | 'Heal' | 'Banish';
 
-export const abilitiesMap: { [key: string]: CardAbility } = {
+export const actionAbilitiesMap: { [key: string]: CardAbility } = {
   fireball: {
     name: 'Fireball',
     usedAgainstOpponentDescription: 'You throw a fireball at your opponent.',
@@ -26,5 +26,34 @@ export const abilitiesMap: { [key: string]: CardAbility } = {
       '50% chance to send your opponent to a distant location on the map on hit.',
     usedAgainstSelfDescription:
       '50% chance to send yourself to a distant location on the map.', // For example, the player could drink a banishment potion.
+  },
+};
+
+export type PassiveCardAbilityKey =
+  | 'Poison'
+  | 'Regenerate'
+  | 'Reflect'
+  | 'MagikaRegen';
+
+export const passiveAbiliiesMap: { [key: string]: CardAbility } = {
+  poison: {
+    name: 'Poison',
+    usedAgainstOpponentDescription: 'You poison your opponent.',
+    usedAgainstSelfDescription: 'You poison yourself.',
+  },
+  regenerate: {
+    name: 'Regenerate',
+    usedAgainstOpponentDescription: 'You regenerate your opponent.',
+    usedAgainstSelfDescription: 'You regenerate yourself.',
+  },
+  reflect: {
+    name: 'Reflect',
+    usedAgainstOpponentDescription: 'You reflect your opponent.',
+    usedAgainstSelfDescription: 'You reflect yourself.',
+  },
+  magikaRegen: {
+    name: 'Magika Regen',
+    usedAgainstOpponentDescription: "You regenerate your opponent's magika.",
+    usedAgainstSelfDescription: 'You regenerate your magika.',
   },
 };
