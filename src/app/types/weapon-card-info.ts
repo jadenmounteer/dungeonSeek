0badb55cfde1617abf27618ab689b46e4f33c070
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2024-04-04 17:15:29 -0600

    Add commnet

--- src/app/types/weapon-card-info.ts
+++ src/app/types/weapon-card-info.ts
@@ -9,7 +9,7 @@ export interface WeaponCardInfo {
   fullPrice: number; // If everyone was honest in Tesslandia, they would pay this price. You'll be lucky if you get 20% of this price when selling. Shopkeepers are greedy.
   rewardType: CardRewardType;
   discardAfterUse: boolean;
-  quantity: number; // Magical weapons were forged by sorcerers long ago and cannot be replicated. There are only a few in existence.
+  quantity: number; // Magical weapons were forged by the sorcerers of old and the knowledge has been lost. There are only a few in existence.
   stats: WeaponCardStats;
   cardAbility: CardAbilityKey;
 }
