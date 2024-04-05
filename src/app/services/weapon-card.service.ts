4fe1416e99c21f23344f5105de86bb7a412fc746
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2024-04-05 08:39:47 -0600

    Begin adding the weapon card service

--- src/app/services/weapon-card.service.ts
+++ src/app/services/weapon-card.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
 import { CardService } from './card.service';
+import { Subscription } from 'rxjs';
+import { CardDeck } from '../types/card-deck';
+import { WeaponCardInfo } from '../types/weapon-card-info';
 
 @Injectable({
   providedIn: 'root',
 })
 export class WeaponCardService {
+  private weaponDeckSub: Subscription | undefined;
+  private weaponDeck: CardDeck[] = [];
+
+  // Map to hold the json data for the cards
+  private weaponCardsInfo: Map<string, WeaponCardInfo> = new Map();
+
   constructor(private cardService: CardService) {}
 }
