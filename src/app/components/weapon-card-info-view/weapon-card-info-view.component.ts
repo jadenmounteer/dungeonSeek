58a4690adc8e71437a521cc03410c4d0d33d51f4
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2024-04-05 11:14:10 -0600

    Fix some menu things

--- src/app/components/weapon-card-info-view/weapon-card-info-view.component.ts
+++ src/app/components/weapon-card-info-view/weapon-card-info-view.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { WeaponCardInfo } from '../../types/weapon-card-info';
+import { Outcome } from '../../types/card-deck';
+import { CommonModule } from '@angular/common';
+import { MenuUnderlineComponent } from '../menu-underline/menu-underline.component';
 
 @Component({
   selector: 'app-weapon-card-info-view',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MenuUnderlineComponent],
   templateUrl: './weapon-card-info-view.component.html',
-  styleUrl: './weapon-card-info-view.component.scss'
+  styleUrl: './weapon-card-info-view.component.scss',
 })
 export class WeaponCardInfoViewComponent {
+  @Input() public card: WeaponCardInfo | undefined;
+  @Output() public closeCard = new EventEmitter<any>();
 
+  protected onCloseCard() {
+    this.closeCard.emit();
+  }
 }
