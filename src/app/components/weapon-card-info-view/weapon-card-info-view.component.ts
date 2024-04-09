3bda06767b84bd013281e44acccb2368527f597a
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2024-04-09 16:15:04 -0600

    Grab the weapon ability to display to user

--- src/app/components/weapon-card-info-view/weapon-card-info-view.component.ts
+++ src/app/components/weapon-card-info-view/weapon-card-info-view.component.ts
@@ -1,8 +1,8 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { WeaponCardInfo } from '../../types/weapon-card-info';
-import { Outcome } from '../../types/card-deck';
 import { CommonModule } from '@angular/common';
 import { MenuUnderlineComponent } from '../menu-underline/menu-underline.component';
+import { CardAbility, actionAbilitiesMap } from '../../types/card-ability';
 
 @Component({
   selector: 'app-weapon-card-info-view',
@@ -11,9 +11,21 @@ import { MenuUnderlineComponent } from '../menu-underline/menu-underline.compone
   templateUrl: './weapon-card-info-view.component.html',
   styleUrl: './weapon-card-info-view.component.scss',
 })
-export class WeaponCardInfoViewComponent {
+export class WeaponCardInfoViewComponent implements OnInit {
   @Input() public card: WeaponCardInfo | undefined;
   @Output() public closeCard = new EventEmitter<any>();
+  protected weaponAbility: CardAbility | undefined;
+
+  ngOnInit(): void {
+    if (this.card?.cardAbility) {
+      this.weaponAbility = this.setWeaponAbility(this.card.cardAbility);
+    }
+  }
+
+  private setWeaponAbility(abilityName: string): CardAbility {
+    // Get the card ability according to the map
+    return actionAbilitiesMap[abilityName];
+  }
 
   protected onCloseCard() {
     this.closeCard.emit();
