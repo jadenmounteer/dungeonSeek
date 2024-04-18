9488c5050888eeac0444e6003997100a0f01c77d
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2024-04-18 16:25:44 -0600

    Move profile image to menu

--- src/app/components/character-menu/character-menu.component.ts
+++ src/app/components/character-menu/character-menu.component.ts
@@ -2,11 +2,18 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Character } from '../../types/character';
 import { MenuComponent } from '../menu/menu.component';
 import { MenuUnderlineComponent } from '../menu-underline/menu-underline.component';
+import { CharacterProfileImageComponent } from '../character-profile-image/character-profile-image.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-character-menu',
   standalone: true,
-  imports: [MenuComponent, MenuUnderlineComponent],
+  imports: [
+    MenuComponent,
+    MenuUnderlineComponent,
+    CharacterProfileImageComponent,
+    CommonModule,
+  ],
   templateUrl: './character-menu.component.html',
   styleUrl: './character-menu.component.scss',
 })
