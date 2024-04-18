22df94d686f70d4f50c42fb888a93b9e92522395
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2024-04-18 16:16:49 -0600

    Make close menu button work

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { fadeIn } from '../../animations/fade-in-animation';
 import { fadeOut } from '../../animations/fade-out-animation';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.scss',
   animations: [fadeIn, fadeOut],
 })
-export class MenuComponent {}
+export class MenuComponent {
+  @Output() closeMenu = new EventEmitter<any>();
+  @Input() showCloseMenu: boolean = false;
+
+  protected onCloseMenu() {
+    this.closeMenu.emit();
+  }
+}
