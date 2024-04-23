b8cf22606ea5336e872a171d7d7ce8efde82d2de
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2024-04-23 08:25:11 -0600

    Add get weapon card info method

--- src/app/components/character-menu-weapon-menu/character-menu-weapon-menu.component.ts
+++ src/app/components/character-menu-weapon-menu/character-menu-weapon-menu.component.ts
@@ -3,6 +3,8 @@ import { fadeIn } from '../../animations/fade-in-animation';
 import { fadeOut } from '../../animations/fade-out-animation';
 import { MenuComponent } from '../menu/menu.component';
 import { CharacterMenuEquipment } from '../../types/character';
+import { WeaponCardService } from '../../services/weapon-card.service';
+import { WeaponCardInfo } from '../../types/weapon-card-info';
 
 @Component({
   selector: 'app-character-menu-weapon-menu',
@@ -16,9 +18,27 @@ export class CharacterMenuWeaponMenuComponent implements OnInit {
   @Output() closeMenu = new EventEmitter<any>();
   @Input() weaponEquipment!: CharacterMenuEquipment;
 
+  protected weaponCardInfo: WeaponCardInfo | undefined;
+
+  constructor(private weaponCardService: WeaponCardService) {}
+
   ngOnInit(): void {
     if (!this.weaponEquipment) {
       throw new Error('Weapon Equipment is required');
+    }
+
+    this.getWeaponCardInfo();
+  }
+
+  private getWeaponCardInfo() {
+    this.weaponCardInfo = this.weaponCardService.getCardInfo(
+      this.weaponEquipment.cardName
+    );
+
+    if (!this.weaponCardInfo) {
+      throw new Error(
+        `${this.weaponEquipment.cardName} card not found in weapon deck`
+      );
     }
   }
 
