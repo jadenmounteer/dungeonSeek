d483f514598042218428f11ea7859bc37568b0fc
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2024-04-25 22:13:53 -0600

    Get rid of event img url

--- src/app/components/game-card/game-card.component.ts
+++ src/app/components/game-card/game-card.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { EventCardInfo } from '../../types/event-card';
+
 import { WeaponCardInfo } from '../../types/weapon-card-info';
 import { ItemCardInfo } from '../../types/item-card-info';
 import { CommonModule } from '@angular/common';
@@ -12,7 +12,7 @@ import { CommonModule } from '@angular/common';
   styleUrl: './game-card.component.scss',
 })
 export class GameCardComponent implements OnInit {
-  @Input() card: EventCardInfo | WeaponCardInfo | ItemCardInfo | undefined; // TODO I can make this only require the image url
+  @Input() card: WeaponCardInfo | ItemCardInfo | undefined; // TODO I can make this only require the image url
   @Input() gold: boolean = false; // If this is a gold card, show the gold icon
   @Input() equipped: boolean = false; // If equipment is equipped.
   @Input() cardJustFound: boolean = false; // If the card was just found, style it differently
