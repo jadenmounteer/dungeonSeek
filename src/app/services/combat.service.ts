84bc2d90a55b97e7371d81cc53fcacb6aad58ce4
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2024-05-02 16:41:12 -0600

    Add team properties

--- src/app/services/combat.service.ts
+++ src/app/services/combat.service.ts
@@ -4,6 +4,8 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class CombatService {
+  #team1 = []; // The team of the current player and their party members
+  #team2 = []; // The team of the current enemies
   constructor() {}
 
   public startCombat(): void {}
