90ae30783ab8697c2363fff368307bddd1f48fee
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2024-05-03 08:25:31 -0600

    Begin adding outcome service

---
+++ src/app/services/outcome.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { Outcome } from '../types/Outcome';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class OutcomeService {
+  // Strategy pattern map for mapping the outcome to the function that will handle the outcome.
+  #outcomeStrategies = new Map<Outcome, () => void>([
+    [Outcome.FIND_EASY_LOOT, () => console.log('Found easy loot')],
+  ]);
+
+  constructor() {}
+
+  public makeChoice(outcome: Outcome): void {
+    const strategy = this.#outcomeStrategies.get(outcome);
+    if (strategy) {
+      strategy();
+    } else {
+      // Handle unknown outcome.
+      console.error('No strategy found for outcome: ', outcome);
+    }
+  }
+}
