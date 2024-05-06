1a399af6e27ea44184f4ae09bff6e98a7fde72dc
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2024-05-06 17:11:06 -0600

    Begin addNPCToGameSession methods

--- src/app/services/npc.service.ts
+++ src/app/services/npc.service.ts
@@ -1,10 +1,13 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import { NPC } from '../types/npc';
+import { Firestore, addDoc, collection } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root',
 })
 export class NpcService {
+  #firestore = inject(Firestore);
+
   #npcs: NPC[] = [];
 
   constructor() {}
@@ -15,5 +18,16 @@ export class NpcService {
 
   public getNpcs(): NPC[] {
     return this.#npcs;
+  }
+
+  public addNpcToGameSession(gameSessionId: string, newNpc: NPC): Promise<any> {
+    const collectionRef = collection(
+      this.#firestore,
+      'game-sessions',
+      gameSessionId,
+      'npcs'
+    );
+
+    return addDoc(collectionRef, newNpc);
   }
 }
