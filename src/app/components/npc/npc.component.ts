9f9c5d0b3cdd902febdd8d6ae10e94909539ac0a
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2024-05-06 17:01:15 -0600

    Create initial npc component and service

---
+++ src/app/components/npc/npc.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+/**
+ * This component is responsible for rendering an NPC and handling its interactions with the world.
+ */
+@Component({
+  selector: 'app-npc',
+  standalone: true,
+  imports: [],
+  templateUrl: './npc.component.html',
+  styleUrl: './npc.component.scss',
+})
+export class NpcComponent {}
