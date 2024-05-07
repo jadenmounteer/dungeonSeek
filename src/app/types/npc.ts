a7381b5936606cdc9add72aa03ff5bcee7e8fdf4
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2024-05-07 07:55:26 -0600

    Rename types

--- src/app/types/npc.ts
+++ src/app/types/npc.ts
@@ -1,13 +1,13 @@
 import { LocationNode } from '../services/location-service';
 import { CardRewardType } from './card-reward-type';
 
-export interface NPC {
+export interface Npc {
   id: string; // The firebase id
   cardId: number; // The id of the card in the card deck. Used to fetch the info from the card deck
-  npcStats: NPCStats;
+  npcStats: NpcStats;
 }
 
-export interface NPCCardInfo {
+export interface NpcCardInfo {
   cardId: number;
   name: string; // This is the identifier for the npc. It is used to fetch the npc from the card deck
   rightFacingImgUrl: string;
@@ -17,7 +17,7 @@ export interface NPCCardInfo {
 
 // These npc stats are added to the npc when it is created and set to the database.
 // This is the info that is stored in the db. Everything else concerning the npc is fetched from JSON.
-export interface NPCStats {
+export interface NpcStats {
   rewardTypeForDefeatingNpc: CardRewardType;
   experiencePointsForDefeatingNpc: number;
   level: number;
