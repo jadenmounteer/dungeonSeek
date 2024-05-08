7b595019594dd724318f60010942292d73bb4eb7
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2024-05-08 17:07:59 -0600

    Grab the npc display info

--- src/app/components/npc/npc.component.ts
+++ src/app/components/npc/npc.component.ts
@@ -1,6 +1,7 @@
-import { Component, InputSignal, input } from '@angular/core';
-import { Npc } from '../../types/npc';
+import { Component, InputSignal, OnInit, inject, input } from '@angular/core';
+import { Npc, NpcDisplayInfo } from '../../types/npc';
 import { CommonModule } from '@angular/common';
+import { NpcService } from '../../services/npc.service';
 
 /**
  * This component is responsible for rendering an NPC and handling its interactions with the world.
@@ -12,6 +13,19 @@ import { CommonModule } from '@angular/common';
   templateUrl: './npc.component.html',
   styleUrl: './npc.component.scss',
 })
-export class NpcComponent {
+export class NpcComponent implements OnInit {
+  public npcDisplayInfo!: NpcDisplayInfo;
+
+  private npcService: NpcService = inject(NpcService);
+
   public npc: InputSignal<Npc> = input.required();
+
+  public ngOnInit(): void {
+    const info = this.npcService.getCardInfo(this.npc().npcType);
+    if (info) {
+      this.npcDisplayInfo = info;
+    } else {
+      throw new Error(`${this.npc().npcType} card not found in npc deck`);
+    }
+  }
 }
--- src/app/services/npc.service.ts
+++ src/app/services/npc.service.ts
@@ -1,20 +1,46 @@
 import { Injectable, inject } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Npc } from '../types/npc';
+import { Npc, NpcDisplayInfo } from '../types/npc';
 import {
   Firestore,
   addDoc,
   collection,
   collectionData,
 } from '@angular/fire/firestore';
+import { CardService } from './card.service';
+import { CardDeck, DeckName } from '../types/card-deck';
 
 @Injectable({
   providedIn: 'root',
 })
 export class NpcService {
   #firestore: Firestore = inject(Firestore);
+  private cardService: CardService = inject(CardService);
 
-  constructor() {}
+  // Map to hold the json data for the cards
+  private npcDisplayInfo: Map<string, NpcDisplayInfo> = new Map();
+
+  constructor() {
+    // We don't handle NPC cards like other card decks because specific npcs come from event cards.
+    // So, we fetch the data from JSON when the user enters the game session.
+    // They are then stored in a map for easy access.
+    this.fetchCardInfoFromJSON();
+  }
+
+  public async fetchCardInfoFromJSON(): Promise<void> {
+    const cards = (await this.cardService.fetchCardsInfoByDeck(
+      DeckName.BASE_GAME_NPCS
+    )) as NpcDisplayInfo[];
+    const mapOfCardNames = new Map<string, NpcDisplayInfo>();
+    cards.forEach((card) => {
+      mapOfCardNames.set(card.npcType, card);
+    });
+    this.npcDisplayInfo = mapOfCardNames;
+  }
+
+  public getCardInfo(cardName: string): NpcDisplayInfo | undefined {
+    return this.npcDisplayInfo.get(cardName);
+  }
 
   public getNPCsInGameSession(gameSessionID: string): Observable<Npc[]> {
     const collectionRef = collection(
