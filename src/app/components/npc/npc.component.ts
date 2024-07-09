2e6ceb93dffa698cd1506a646c06a3128cb192ad
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2024-07-09 16:53:46 -0600

    Make npc health be animated

--- src/app/components/npc/npc.component.ts
+++ src/app/components/npc/npc.component.ts
@@ -32,32 +32,8 @@ export class NpcComponent implements OnInit {
 
   public inCombat = false;
 
-  public healthPercentage: number = 100;
-
   public ngOnInit(): void {
     this.inCombat = this.npc().combatSessionID !== null;
-  }
-
-  ngAfterViewInit(): void {
-    this.updateHealthBar();
-  }
-
-  ngOnChanges(changes: SimpleChanges): void {
-    if (changes['npc']) {
-      this.updateHealthBar();
-    }
-  }
-
-  public updateHealthBar(): void {
-    const npcStats = this.npc().npcStats;
-
-    this.healthPercentage =
-      (npcStats.health.current / npcStats.health.total) * 100;
-    const healthBar = document.getElementById('npcHealthBar');
-
-    if (healthBar) {
-      healthBar.style.width = `${this.healthPercentage}%`;
-    }
   }
 
   public selectNpc(): void {
