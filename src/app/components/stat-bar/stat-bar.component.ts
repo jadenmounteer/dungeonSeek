460bcc6d3666b5887c209791d831ce45ef9bfc53
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2024-07-09 16:45:23 -0600

    Make stat bar change

--- src/app/components/stat-bar/stat-bar.component.ts
+++ src/app/components/stat-bar/stat-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, SimpleChanges } from '@angular/core';
 import { CharacterStat } from '../../types/character';
 import { CommonModule } from '@angular/common';
 
