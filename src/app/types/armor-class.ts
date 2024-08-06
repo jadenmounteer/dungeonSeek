34ffe2175c838601bfcd92441fb0c405d5d89d56
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2024-08-06 16:27:12 -0600

    Add comment

--- src/app/types/armor-class.ts
+++ src/app/types/armor-class.ts
@@ -1,5 +1,6 @@
 export enum ArmorClass {
   // Multiples of 6 because we use d6s for combat.
+  // The more attack dice weapons and spells have, the more effective they are against your opponent.
   NONE = 1,
   LIGHT = 6,
   MEDIUM = 12,
