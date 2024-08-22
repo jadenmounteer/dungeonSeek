919c90f0c56fe820d5b38459e217fb9f1870f9c2
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2024-08-22 15:25:11 -0600

    Begin adding npc type

--- src/app/types/npcs/wolf.ts
+++ src/app/types/npcs/wolf.ts
@@ -1,18 +1,18 @@
 import { ArmorClass } from '../armor-class';
 import { Npc, NpcData } from './npc';
 
-export class Bandit extends Npc {
+export class Wolf extends Npc {
   constructor(npcData: NpcData) {
     super(npcData);
 
-    this.rightFacingImgUrl = 'assets/game-pieces/bandit/bandit-right.png';
-    this.leftFacingImgUrl = 'assets/game-pieces/bandit/bandit-left.png';
+    this.rightFacingImgUrl = 'assets/game-pieces/wolf/wolf-right.png';
+    this.leftFacingImgUrl = 'assets/game-pieces/wolf/wolf-left.png';
     this.npcStats = npcData.npcStats ?? {
       health: {
         current: 1,
         total: 10,
       },
-      armorClass: ArmorClass.LIGHT,
+      armorClass: ArmorClass.NONE,
     };
   }
 }
